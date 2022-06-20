c4db3bde2304323d00022c08b6dc299bd7b4d699
harshparmar0 <82783770+harshparmar0@users.noreply.github.com>
2022-06-20 15:14:40 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -65,7 +65,7 @@ async def play(_, message: Message):
     try:
         user = await USER.get_me()
     except:
-        user.first_name = "Anonymous"
+        user.first_name = "Harsh"
     usar = user
     wew = usar.id
     try:
